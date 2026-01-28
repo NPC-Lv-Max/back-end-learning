8be78e70022cbdad157050f89919b8872707578d
oil <oilxdcggg@gmail.com>
2026-01-28 15:44:33 +0700

    v2

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3001; 
+
+app.use(express.json());
+
+let departments = [
+    { id: 1, name: ' IT' },
+    { id: 2, name: 'DB' },
+];
 app.get('/', (req, res) => {
     res.send( "My. Dnan Namjun" );
 });
+app.get('/departments', (req, res) => {
+    res.send(departments);
+});
+app.post('/departments', (req, res) => {
+    let newDepartment = {
+        id: departments.length + 1,
+        name: req.body.name,
+    };
+    departments.push(newDepartment);
+    res.send(departments);
+});
+
 app.listen(port, () => {
     console.log(`Server Started on Port ${port}`);
 });
